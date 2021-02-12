0cfae5a363641f174c0f47e257169f2ffab1dc29
Zaki-dotcom <zakaria.sayad69@gmail.com>
2021-02-12 21:29:33 +0100

    first commit

---
+++ src/main/java/com/todos/pages/TodoPage.java
@@ -0,0 +1,36 @@
+package com.todos.pages;
+
+import java.io.IOException;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import com.todos.utils.BasePage;
+
+public class TodoPage extends BasePage {
+
+	public TodoPage() throws IOException {
+		
+		PageFactory.initElements(driver, this);
+	}
+	
+	/*Locators*/
+	final static String INPUT_TEXT = "//input[@ng-model='newTodo']";
+	
+	/*@FindBy*/
+	//@FindBy(id="menu_NewSystem")
+	//private WebElement menuNewSystem;
+	@FindBy(how = How.XPATH, using = INPUT_TEXT)
+	public static WebElement inputText;
+
+	/*Methods*/
+	public void submitTodo(String todo) {
+		writeText(inputText, todo);
+		inputText.sendKeys(Keys.ENTER);
+	}
+}
+
+
